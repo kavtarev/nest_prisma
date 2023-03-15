8e4baf05e1e3a776b03f69138fb4b8927e50c9a3
ivan <ivan.kavtarev@mail.ru>
2023-03-15 23:27:49 +0300

    play w streams

---
+++ streams.js
@@ -0,0 +1,45 @@
+const { Readable, Writable } = require('stream');
+const { appendFile } = require('fs/promises');
+
+class myReadable extends Readable {
+  max = 10;
+  index = 0;
+
+  constructor(props) {
+    super(props);
+  }
+
+  _read() {
+    this.index += 1;
+    if (this.index > this.max) {
+      this.push(null);
+    } else {
+      const buf = Buffer.from(`${this.index}`, 'utf8');
+      let res = this.push(buf);
+
+      console.log(`current index ${this.index}, push ${res}`);
+    }
+  }
+}
+
+async function hui() {
+  const readable = new myReadable({ highWaterMark: 2 });
+
+  readable.on('data', (chunk) => {
+    console.log(chunk.toString());
+  });
+}
+
+hui();
+// console.log(`Received: ${readable.read().toString()}`);
+
+// class myWritable extends Writable {
+//   async _write(chunk, cod, cb) {
+//     await appendFile('some.txt', chunk.toString());
+//     cb();
+//   }
+// }
+
+// const writable = new myWritable({ highWaterMark: 2 });
+
+// readable.pipe(writable);
--- test.js
+++ test.js
@@ -1,40 +1,57 @@
 const { createWriteStream, createReadStream } = require('fs');
-const { finished } = require('stream/promises');
-const { Readable } = require('stream');
 
 async function hui() {
-  let headersList = {
-    Accept: '*/*',
-    'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
-    'Content-Type': 'application/json',
-  };
-
-  let bodyContent = JSON.stringify({
-    name: 'ivan',
-    email: 'email',
-  });
-
-  let response = await fetch('http://localhost:3000/download-pdf', {
-    method: 'POST',
-    body: bodyContent,
-    headers: headersList,
-  });
-
-  const writeStream = createWriteStream('write.pdf', {
+  const stream = createReadStream('some.html', {
     encoding: 'utf-8',
+    highWaterMark: 16000,
   });
 
-  const readStream = createReadStream('test.js', {
-    encoding: 'utf-8',
+  stream.on('data', (c) => {
+    console.log(c.toString());
+    console.log(c.length);
   });
-  console.log(1);
-  const data = await finished(readStream.pipe(writeStream));
-  console.log(266, data);
-}
 
-async function dui() {
-  let res = await fetch('http://localhost:3000/get');
-  console.log(await res.body.pipeTo());
+  // for await (const buf of stream) {
+  //   console.log(223);
+  // }
 }
-
 hui();
+// const { finished } = require('stream/promises');
+// const { Readable } = require('stream');
+
+// async function hui() {
+//   let headersList = {
+//     Accept: '*/*',
+//     'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
+//     'Content-Type': 'application/json',
+//   };
+
+//   let bodyContent = JSON.stringify({
+//     name: 'ivan',
+//     email: 'email',
+//   });
+
+//   let response = await fetch('http://localhost:3000/download-pdf', {
+//     method: 'POST',
+//     body: bodyContent,
+//     headers: headersList,
+//   });
+
+//   const writeStream = createWriteStream('write.pdf', {
+//     encoding: 'utf-8',
+//   });
+
+//   const readStream = createReadStream('test.js', {
+//     encoding: 'utf-8',
+//   });
+//   console.log(1);
+//   const data = await finished(readStream.pipe(writeStream));
+//   console.log(266, data);
+// }
+
+// async function dui() {
+//   let res = await fetch('http://localhost:3000/get');
+//   console.log(await res.body.pipeTo());
+// }
+
+// hui();
